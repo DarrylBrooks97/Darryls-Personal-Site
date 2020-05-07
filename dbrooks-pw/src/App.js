eba5152e441a69b40e202acdd0e70110579af515
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-05-07 17:53:45 -0400

    Init commit

--- dbrooks-pw/src/App.js
+++ dbrooks-pw/src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-// import './App.css';
-
-import './App.css';
+import React from "react";
+// import Home from "./Home";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import ReactRotatingText from "react-rotating-text";
+import "./App.css";
 
 function App() {
   return (
-    <header>
-      <nav>
-        <ul className="page-links">
-          <li><a className="nav-links" href="#"> About Me </a></li>
-          <li><a className="nav-links" href="#">Resume</a></li>
-          <li><a className="nav-links" href="#">Interests</a></li>
-        </ul>
-      </nav>
-    </header>
+    <div>
+      <p>Darryl Brooks II</p>
+      <p3>is</p3>
+      <p1>
+        <ReactRotatingText
+          items={[
+            'cout<<"Innovative";',
+            'cout<<"Relentless";',
+            'cout<<"Ambitious";',
+          ]}
+        />
+      </p1>
+      <Button variant="contained">Check out my life</Button>
+    </div>
   );
 }
 
