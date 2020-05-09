33c8ca25a68b8077125b768927697fe16d5fda3a
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-05-09 19:52:51 -0400

    Website Navagation is finished. Still developing for mobile

--- dbrooks-pw/src/Home.js
+++ dbrooks-pw/src/Home.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import "./home.css";
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 function Home() {
-  return <div>Home page</div>;
+  const classes = useStyles();
+
+  function navSlide() {
+    const burger = document.querySelector("burger");
+    const nav = document.querySelector("list");
+    console.log(burger);
+    if (burger) {
+      burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+      });
+    }
+  }
+  return (
+    <div className="nav-bar">
+      <h className="name">Darryl Brooks</h>
+      <div className="burger">
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+      <ul className="list">
+        <li>
+          <Button size="medium" className={classes.margin}>
+            About
+          </Button>
+        </li>
+        <li>
+          <Button size="medium" className={classes.margin}>
+            Tech Experience
+          </Button>
+        </li>
+        <li>
+          <Button size="medium" className={classes.margin}>
+            Contact
+          </Button>
+        </li>
+      </ul>
+      {navSlide()}
+    </div>
+  );
 }
 
 export default Home;
---
+++ dbrooks-pw/src/LandingPage.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
+import ReactRotatingText from "react-rotating-text";
+import { Redirect, useHistory } from "react-router-dom";
+
+function LandingPage() {
+  const [redirect, setRedirect] = useState(false);
+  let history = useHistory();
+
+  function routeChange() {
+    if (redirect) {
+      history.push("/");
+      return <Redirect to="/home" />;
+    }
+  }
+
+  return (
+    <div>
+      <p>Darryl Brooks II</p>
+      <p3>is</p3>
+      <p1>
+        <ReactRotatingText
+          items={[
+            'cout<<"Innovative";',
+            'cout<<"Relentless";',
+            'cout<<"Ambitious";',
+          ]}
+        />
+      </p1>
+      {routeChange()}
+      <div className="button">
+        <Button
+          variant="contained"
+          onClick={() => {
+            setRedirect(true);
+          }}
+          style={{
+            backgroundColor: "#e2c044",
+            textTransform: "capitalize",
+            color: "#3f4043",
+            fontFamily: "Montserrat",
+          }}
+        >
+          Check out my life
+        </Button>
+      </div>
+    </div>
+  );
+}
+export default LandingPage;
