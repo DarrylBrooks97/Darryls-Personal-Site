6d2e20a035cf6de1379faf87afd3f40f1d114c60
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-05-23 14:38:24 -0400

    Needs more styling

--- dbrooks-pw/src/Home.js
+++ dbrooks-pw/src/Home.js
@@ -9,25 +9,61 @@ function Home() {
       <PageNavBar location="/home" />
       <div className="home-div">
         <div className="home-headline">{"cout<<About<<endl"}</div>
-        <div className="h-contents">
-          <div className="home-content">
-            <img
-              className="home-picture"
-              src={require("./images/IMG_1829.JPG")}
-              alt="pic"
-            />
-            <div className="txt-container">
-              <h1 className="about-txt">
-                Darryl Brooks, an upcoming Software Engineer, was born in Tampa,
-                FL however, was raised in Jacksonville, FL. As a child he always
-                had a fasination in sciene & technology, where he was always
-                curious on how computers worked. His parents put him to the test
-                and enrolled him at Robert E. Lee High School for their rigorous
-                Engineering program.
-              </h1>
+        <ul className="h-contents">
+          <li className="home-item">
+            <div className="home-content">
+              <div
+                style={{
+                  width: "60%",
+                }}
+              >
+                <img
+                  className="pictures"
+                  src={require("./images/IMG_1829.JPG")}
+                  alt="pic"
+                />
+              </div>
+              <div className="txt-container">
+                <h1 className="about-txt">
+                  Darryl Brooks, an upcoming Software Engineer, was born in
+                  Tampa, FL however, was raised in Jacksonville, FL. As a child
+                  he always had a fasination in sciene & technology, where he
+                  was always curious on how computers worked. His parents put
+                  him to the test and enrolled him at Robert E. Lee High School
+                  for their rigorous Engineering program.
+                </h1>
+              </div>
             </div>
-          </div>
-        </div>
+          </li>
+          <li className="home-item">
+            <div className="home-content">
+              <div style={{ width: "40%" }}>
+                <h1 className="about-txt">Lists of Interests</h1>
+              </div>
+              <div style={{ width: "60%" }}>
+                <img
+                  className="pictures"
+                  src={require("./images/cleanPic.JPG")}
+                  alt="pic"
+                />
+              </div>
+            </div>
+          </li>
+          <li className="home-item">
+            <div className="home-content">
+              <div style={{ width: "60%" }}>
+                <img
+                  className="pictures"
+                  src={require("./images/IMG_4990.jpg")}
+                  alt="pic"
+                />
+              </div>
+              <div style={{ width: "45%", height: "50%" }}>
+                <h1 className="about-txt">Fun Facts</h1>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
