9797655087003ceaa34176771c26307c204aede5
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-06-04 22:36:05 -0400

    Contact Form sends emails but now needs responsive styling

--- dbrooks-pw/src/Contact.js
+++ dbrooks-pw/src/Contact.js
@@ -2,18 +2,9 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PageNavBar from "../src/Components/PageNavBar";
 import "./Contact.css";
-import { TextField } from "@material-ui/core";
-import FormControl from "@material-ui/core/FormControl";
-
-import {
-  fade,
-  withStyles,
-  makeStyles,
-  createMuiTheme,
-} from "@material-ui/core/styles";
-import InputBase from "@material-ui/core/InputBase";
-import InputLabel from "@material-ui/core/InputLabel";
-import { green } from "@material-ui/core/colors";
+import { FormControl, Button, InputBase, InputLabel } from "@material-ui/core";
+import { fade, withStyles, makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
 
 const BootstrapInput = withStyles((theme) => ({
   root: {
@@ -28,7 +19,6 @@ const BootstrapInput = withStyles((theme) => ({
     color: "black",
     border: "3px solid #ffff",
     fontSize: 18,
-    width: "auto",
     padding: "10px 12px",
     transition: theme.transitions.create(["border-color", "box-shadow"]),
     // Use the system font instead of the default Roboto font.
@@ -53,13 +43,36 @@ const BootstrapInput = withStyles((theme) => ({
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    flexWrap: "wrap",
+    // display: "flex",
+    // justifyContent: "space-around",
   },
   margin: {
     // margin: theme.spacing(1),
   },
 }));
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  axios
+    .post("http://localhost:8080/api/form", {
+      name: document.getElementById("name").value,
+      mail: document.getElementById("mail").value,
+      message: document.getElementById("message").value,
+    })
+    .then(() => {
+      alert("Submited!");
+      resetForm();
+    })
+    .catch((e) => {
+      alert(e);
+    });
+}
+function resetForm() {
+  document.getElementById("name").value = "";
+  document.getElementById("mail").value = "";
+  document.getElementById("message").value = "";
+}
 
 function TechX() {
   const classes = useStyles();
@@ -70,39 +83,75 @@ function TechX() {
       <div className="contact-div">
         <div className="contact-headline">{"Contact"}</div>
         <div className="contact-title">{"Lets Chat"}</div>
-        <div className="contact-info">
-          {/* <form className={classes.root} noValidate> */}
-          <FormControl className={classes.margin}>
-            <InputLabel shrink style={{ color: "#e2c044" }}>
-              yourName
-            </InputLabel>
-            <BootstrapInput
-              placeholder="cin>>yourName"
-              style={{ color: "white", width: "100vh" }}
-            />
-          </FormControl>
-          <FormControl className={classes.margin}>
-            <InputLabel shrink style={{ color: "#e2c044" }}>
-              yourEmail
-            </InputLabel>
-            <BootstrapInput
-              placeholder="cin>>yourEmail"
-              style={{ color: "black" }}
-            />
-          </FormControl>
-          {/* </form> */}
-        </div>
-        <div className="message-div">
-          <FormControl>
-            <InputLabel shrink style={{ color: "#e2c044" }}>
-              yourMessage
-            </InputLabel>
-            <BootstrapInput
-              placeholder="cin>>yourMessage"
-              style={{ color: "white" }}
-            />
-          </FormControl>
-        </div>
+        <form className={classes.root} onSubmit={(e) => handleSubmit(e)}>
+          <div className="contact-info">
+            <FormControl className={classes.margin}>
+              <InputLabel shrink style={{ color: "#e2c044" }}>
+                yourName
+              </InputLabel>
+              <BootstrapInput
+                type="text"
+                id="name"
+                required
+                placeholder="cin>>yourName;"
+                style={{ color: "white", width: "120%" }}
+              />
+            </FormControl>
+            <FormControl className={classes.margin}>
+              <InputLabel shrink style={{ color: "#e2c044" }}>
+                yourEmail
+              </InputLabel>
+              <BootstrapInput
+                type="email"
+                id="mail"
+                required
+                placeholder="cin>>yourEmail;"
+                style={{ color: "black", width: "120%" }}
+              />
+            </FormControl>
+          </div>
+
+          <div className="message-div">
+            <FormControl>
+              <InputLabel shrink style={{ color: "#e2c044" }}>
+                yourMessage
+              </InputLabel>
+              <BootstrapInput
+                type="text"
+                required
+                placeholder="cin>>yourMessage;"
+                style={{
+                  color: "white",
+                  width: "300%",
+                }}
+                id="message"
+                multiline
+                rows={3}
+              />
+            </FormControl>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              margin: "1% 0px 3% 30%",
+              textAlign: "center",
+            }}
+          >
+            <Button
+              variant="contained"
+              type="submit"
+              style={{
+                backgroundColor: "#e2c044",
+                textTransform: "capitalize",
+                color: "#3f4043",
+                fontFamily: "Montserrat",
+              }}
+              size="large"
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
