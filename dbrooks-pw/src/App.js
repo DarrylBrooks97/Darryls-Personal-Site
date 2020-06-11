30f2e8a49c6e5d22aad3a7ef21494f1cd296539b
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-06-11 18:05:40 -0400

    Ready for Deployment

--- dbrooks-pw/src/App.js
+++ dbrooks-pw/src/App.js
@@ -1,45 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import LandingPage from "./Home";
 import About from "./About";
 import TechX from "./TechX/";
 import Contact from "./Contact";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import { db } from "./firebase";
 
 function App() {
-  const [res, setRes] = useState(undefined);
-
-  const getInfo = () => {
-    db.collection("Mandatum")
-      .doc("FlUukImoonkI8HEKJVkN")
-      .get()
-      .then((resp) => {
-        setRes(resp.data());
-      });
-  };
-  if (res === undefined) {
-    getInfo();
-  }
   return (
     <div>
-      {res !== undefined ? (
-        <Router>
-          <Switch>
-            <Route path="/" exact render={() => <LandingPage />} />
-            <Route path="/home" render={() => <About />} />
-            <Route path="/techX" render={() => <TechX />} />
-            <Route path="/contact" render={() => <Contact info={res} />} />
-          </Switch>
-        </Router>
-      ) : (
-        <div>
-          <img
-            style={{ width: "auto", height: "auto" }}
-            src={require("./images/loading.gif")}
-          />
-        </div>
-      )}
+      <Router basename="/">
+        <Switch>
+          <Route path="/" exact render={() => <LandingPage />} />
+          <Route path="/home" exact render={() => <About />} />
+          <Route path="/techX" exact render={() => <TechX />} />
+          <Route path="/contact" exact render={() => <Contact />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
