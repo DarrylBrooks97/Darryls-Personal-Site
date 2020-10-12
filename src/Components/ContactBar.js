26250efb4f646e9c2a96c86220629c28e4662a98
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-10-11 22:32:37 -0400

    Moved files, created .circleci file and change IG handle

--- src/Components/ContactBar.js
+++ src/Components/ContactBar.js
@@ -33,7 +33,7 @@ function ContactBar() {
         </li>
         <li>
           <a
-            href="https://www.instagram.com/d.brooks97"
+            href="https://www.instagram.com/darryl.codes"
             target="_blank"
             rel="noopener noreferrer"
           >
