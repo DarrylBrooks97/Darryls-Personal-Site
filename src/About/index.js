41b6f15f29bf7e76d31efc2998583140e339877e
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-12-26 13:01:30 -0500

    Updated class name for contact icons

--- src/About/index.js
+++ src/About/index.js
@@ -1,108 +1,106 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import PageNavBar from "../Components/PageNavBar";
-import ContactBar from "../Components/ContactBar";
-import "./home.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PageNavBar from '../Components/PageNavBar';
+import ContactBar from '../Components/ContactBar';
+import './home.css';
 
 function Home() {
-  return (
-    <div style={{ width: "auto" }}>
-      <div className="home">
-        <PageNavBar location="/home" />
-        <div className="home-div">
-          <div className="home-headline">{"About"}</div>
-          <ul className="h-contents">
-            <li className="home-item">
-              <div className="home-content">
-                <div className="home-content-div">
-                  <img
-                    className="pictures"
-                    src={require("../images/IMG_1829.JPG")}
-                    alt="pic"
-                  />
-                </div>
-                <div className="txt-container">
-                  <h1 className="about-txt">
-                    About Me
-                    <hr></hr>
-                    Darryl Brooks, an upcoming Software Engineer, was born in
-                    Tampa, FL byway Jacksonville, FL. As a child he always had a
-                    fascination with science & technology, where he was always
-                    curious on how computers worked. His parents put him to the
-                    test and enrolled him at Robert E. Lee High School for their
-                    rigorous Engineering program. From there he knew he wanted
-                    to use his technical abilities to make a change in the
-                    technology industry. Soon applying for one of the best
-                    universities in the state of Florida for Computer
-                    Engineering,
-                    <a
-                      href="https://www.famu.edu"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Florida A&M University
-                    </a>
-                    . He is now entering his senior year and is actively
-                    applying to full-time Software engineering positions.
-                    <br></br>
-                  </h1>
-                </div>
-              </div>
-            </li>
-            <li className="home-item">
-              <div className="interest">
-                <div className="interest-div">
-                  <h1 className="about-txt">
-                    Personal Interests
-                    <hr></hr>
-                    <ul className="lists">
-                      <li>Full-Stack Development</li>
-                      <li>Aquatics</li>
-                      <li>Basketball</li>
-                      <li>Hiking</li>
-                      <li>Billards</li>
-                    </ul>
-                  </h1>
-                </div>
-                <div className="about-content-div">
-                  <img
-                    className="pictures"
-                    src={require("../images/MS.jpg")}
-                    alt="pic"
-                  />
-                </div>
-              </div>
-            </li>
-            <li className="home-item">
-              <div className="home-content">
-                <div className="fun-fact-div">
-                  <img
-                    className="pictures"
-                    src={require("../images/IMG_4990.jpg")}
-                    alt="pic"
-                  />
-                </div>
-                <div className="about-txt-div">
-                  <h1 className="about-txt">
-                    Fun Facts
-                    <hr></hr>
-                    <ul className="lists">
-                      <li>First Generation College Student</li>
-                      <li>Solves a Rubix Cube within 1 minute</li>
-                      <li>Actively learning Spanish</li>
-                    </ul>
-                  </h1>
-                </div>
-              </div>
-            </li>
-          </ul>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <ContactBar />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div style={{ width: 'auto' }}>
+			<div className='home'>
+				<PageNavBar location='/home' />
+				<div className='home-div'>
+					<div className='home-headline'>{'About'}</div>
+					<ul className='h-contents'>
+						<li className='home-item'>
+							<div className='home-content'>
+								<div className='home-content-div'>
+									<img
+										className='pictures'
+										src={require('../images/IMG_1829.JPG')}
+										alt='pic'
+									/>
+								</div>
+								<div className='txt-container'>
+									<h1 className='about-txt'>
+										About Me
+										<hr></hr>
+										Darryl Brooks, an upcoming Software Engineer, was born in Tampa, FL
+										byway Jacksonville, FL. As a child he always had a fascination with
+										science & technology, where he was always curious on how computers
+										worked. His parents put him to the test and enrolled him at Robert E.
+										Lee High School for their rigorous Engineering program. From there he
+										knew he wanted to use his technical abilities to make a change in the
+										technology industry. Soon applying for one of the best universities in
+										the state of Florida for Computer Engineering,
+										<a
+											href='https://www.famu.edu'
+											target='_blank'
+											rel='noopener noreferrer'
+										>
+											Florida A&M University
+										</a>
+										. He is now entering his senior year and is actively applying to
+										full-time Software engineering positions.
+										<br></br>
+									</h1>
+								</div>
+							</div>
+						</li>
+						<li className='home-item'>
+							<div className='interest'>
+								<div className='interest-div'>
+									<h1 className='about-txt'>
+										Personal Interests
+										<hr></hr>
+										<ul className='lists'>
+											<li>Full-Stack Development</li>
+											<li>Aquatics</li>
+											<li>Basketball</li>
+											<li>Hiking</li>
+											<li>Billards</li>
+										</ul>
+									</h1>
+								</div>
+								<div className='about-content-div'>
+									<img
+										className='pictures'
+										src={require('../images/MS.jpg')}
+										alt='pic'
+									/>
+								</div>
+							</div>
+						</li>
+						<li className='home-item'>
+							<div className='home-content'>
+								<div className='fun-fact-div'>
+									<img
+										className='pictures'
+										src={require('../images/IMG_4990.jpg')}
+										alt='pic'
+									/>
+								</div>
+								<div className='about-txt-div'>
+									<h1 className='about-txt'>
+										Fun Facts
+										<hr></hr>
+										<ul className='lists'>
+											<li>First Generation College Student</li>
+											<li>Solves a Rubix Cube within 1 minute</li>
+											<li>Actively learning Spanish</li>
+										</ul>
+									</h1>
+								</div>
+							</div>
+						</li>
+					</ul>
+					<div style={{ display: 'flex', justifyContent: 'center' }}>
+						<ContactBar />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
--- src/Components/ContactBar.js
+++ src/Components/ContactBar.js
@@ -1,68 +1,62 @@
-import React from "react";
-import "./ContactBar.css";
+import React from 'react';
+import './ContactBar.css';
 
 function ContactBar() {
-  return (
-    <div className="contact-bar">
-      <ul className="social-medias">
-        <li>
-          <a
-            href="https://www.linkedin.com/in/darryl-brooks-ii-a17a46140/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={require("../images/li.png")}
-              alt="linkdin.png"
-              id="linkedin"
-            ></img>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.github.com/DarrylBrooks97"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={require("../images/git.png")}
-              alt="git.png"
-              id="github"
-            ></img>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/darryl.codes"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={require("../images/ig.png")}
-              alt="instagram.png"
-              id="instagram"
-            ></img>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com/DarrylB97"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={require("../images/twitter.png")}
-              alt="twitter.png"
-              id="twitter"
-            ></img>
-          </a>
-        </li>
-      </ul>
-      <div className="copyright">
-        © 2020 Darryl Brooks. All rights reserved.
-      </div>
-    </div>
-  );
+	return (
+		<div className='contact-bar'>
+			<ul className='my-socials'>
+				<li>
+					<a
+						href='https://www.linkedin.com/in/darryl-brooks-ii-a17a46140/'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<img
+							src={require('../images/li.png')}
+							alt='linkdin.png'
+							id='linkedin'
+						></img>
+					</a>
+				</li>
+				<li>
+					<a
+						href='https://www.github.com/DarrylBrooks97'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<img src={require('../images/git.png')} alt='git.png' id='github'></img>
+					</a>
+				</li>
+				<li>
+					<a
+						href='https://www.instagram.com/darryl.codes'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<img
+							src={require('../images/ig.png')}
+							alt='instagram.png'
+							id='instagram'
+						></img>
+					</a>
+				</li>
+				<li>
+					<a
+						href='https://www.twitter.com/DarrylB97'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<img
+							src={require('../images/twitter.png')}
+							alt='twitter.png'
+							id='twitter'
+						></img>
+					</a>
+				</li>
+			</ul>
+			<div className='copyright'>© 2020 Darryl Brooks. All rights reserved.</div>
+		</div>
+	);
 }
 
 export default ContactBar;
